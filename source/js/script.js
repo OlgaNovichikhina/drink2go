c37ad410b3ec9ade6c6eee7cf65b0dd8a67c561c
OlgaNovichikhina <97948178+OlgaNovichikhina@users.noreply.github.com>
2022-10-04 00:32:45 +0300

    блок с преимуществами и фикс свайпера

--- source/js/script.js
+++ source/js/script.js
@@ -15,18 +15,18 @@ navToggle.addEventListener('click', function() {
 
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-});
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    bulletElement: 'button',
+    bulletActiveClass:'swiper-pagination-bullet--active'
+  },
+}
+);
