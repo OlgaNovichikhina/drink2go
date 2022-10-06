057cbae78d293c1462d5c72bef6ddc30cd8bf84b
OlgaNovichikhina <97948178+OlgaNovichikhina@users.noreply.github.com>
2022-10-07 00:11:08 +0300

    footer

--- source/js/script.js
+++ source/js/script.js
@@ -30,3 +30,34 @@ const swiper = new Swiper('.swiper', {
   },
 }
 );
+
+const map = L.map('map-wrapper')
+  .setView({
+    lat: 59.968137,
+    lng: 30.316272,
+  }, 16);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '../images/map-pin.png',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+});
+
+const marker = L.marker(
+  {
+    lat: 59.968137,
+    lng: 30.316272,
+  },
+  {
+    icon: mainPinIcon,
+  },
+);
+
+marker.addTo(map);
